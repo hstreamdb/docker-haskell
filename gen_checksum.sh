9e13cd16556090e2dc0fb7137da414e878763c1a
mu <59917266+4eUeP@users.noreply.github.com>
2021-11-30 14:28:22 +0800

    gen_checksum.sh: add all for all dockerfiles

--- gen_checksum.sh
+++ gen_checksum.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "$0" "<path to checksum file>"
-else
+refresh_checksum () {
     TMP_FILE=$(mktemp)
     cat "$1" | tr -s " " | cut -d" " -f2 | xargs -i bash -c "sha256sum {} >> $TMP_FILE"
     mv $TMP_FILE "$1"
+}
+
+if [ -z "$1" ]; then
+    echo "$0" "[<path to checksum file>|all]"
+elif [ "$1" == "all" ]; then
+    export -f refresh_checksum
+    find checksum/ -type f -name "*.sha256sum" | xargs -I {} bash -c 'refresh_checksum "{}"'
+else
+    refresh_checksum "$1"
 fi
